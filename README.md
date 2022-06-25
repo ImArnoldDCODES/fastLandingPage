faa8e50df6d3e6af5a972014c5a794fdcd7a8196
A.D_codes <86753228+ImArnoldDCODES@users.noreply.github.com>
2022-06-25 18:12:39 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# trying
+just playing around
+
+Trying is free 
+
+So it didnt work out 
+
+Hmmm Why 
+
+Hello can someone explain
---
+++ index.js
@@ -0,0 +1,13 @@
+// const Try = [
+//     {
+//         name: "Lol",
+//         Age: "23"
+
+//     },
+//     {
+//         name: "Second Lol",
+//         Age: "82"
+//     }
+// ]
+
+// console.log(Try)
